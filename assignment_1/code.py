2e5f70c307ba94d827270842993047979511120a
Axel Ólafsson <axelola18@ru.is>
2021-11-23 00:24:39 +0000

    brightness with for loops

--- assignment_1/code.py
+++ assignment_1/code.py
@@ -22,6 +22,17 @@ def reddest(data):
     (_, _, _, point) = cv.minMaxLoc(img_hsv[:, :, 1], mask)
     return point
 
+def brightestRaw(data):
+    gray = cv.cvtColor(data, cv.COLOR_BGR2GRAY)
+    (rows, columns) = gray.shape
+    brightestValue = 0
+    for i in range(rows):
+        for j in range(columns):
+            if (gray[i,j] > brightestValue):
+                brightestPoint = (j, i)
+                brightestValue = gray[i, j]
+    return brightestPoint
+
 def main():
 
     vid = cv.VideoCapture(0)
@@ -30,7 +41,8 @@ def main():
     while True:
         start = time.time()
         _flag, img = vid.read()
-        point = reddest(img)
+        point = brightestRaw(img)
+        nonraw = brightest(img)
         cv.circle(img, point, 9, (0, 0, 255), 2)
         cv.putText(img, 'FPS = '+ str(int(fps)), (10,450), font, 3, (0, 255, 0), 2, cv.LINE_AA)
         cv.imshow('cam', img)
@@ -42,7 +54,8 @@ def main():
         if ch == 27:
             break
     print('Done')
-
+    print(point)
+    print(nonraw)
 
 if __name__ == '__main__':
     print(__doc__)
